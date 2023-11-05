2c3af72b42570674848a0f574fb7e839417b8f82
Anastasia Bielievitina <abielievitina@gmail.com>
2023-11-04 21:53:39 -0400

    minor fix

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,8 +1,8 @@
 export default function Landing() {
     return (
         <div className="my-8 sm:mx-4">
-        <div className="bg-cover justify-center custom-font capitalize text-white flex lg:items-start sm:items-end lg:p-16 sm:p-8 w-full lg:h-[676px] sm:h-[372px] rounded-md">
-            <h1 className="lg:text-3xl sm:text-xl border-b custom-font text-center animate-pulse capitalize">World class auto parts <br /> at your fingertips.</h1>
+        <div className="bg-cover justify-center custom-font capitalize text-white flex items-end md:p-16 sm:p-8 w-full md:h-[676px] sm:h-[372px] rounded-md">
+            <h1 className="md:text-3xl sm:text-xl border-b custom-font text-center animate-pulse capitalize">World class auto parts <br /> at your fingertips.</h1>
         </div>
         </div>
     )
